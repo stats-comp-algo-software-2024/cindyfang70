4a9317c6e0627b22cf555b616fed8efddd2d7b39
Cindy Fang <cindyfang70@gmail.com>
2024-02-05 17:31:09 -0500

    add hiper_glm function and roxygen2 documentation

---
+++ R/hiper_glm.R
@@ -0,0 +1,13 @@
+#' hiper_glm
+#'
+#' @param design a design matrix
+#' @param outcome a vector of outcomes
+#'
+#' @return An object of class `hglm`
+#' @export
+hiper_glm <- function(design, outcome){
+  warning("This is a placeholder until the function is implemented")
+  hglm <- list()
+  class(hglm) <- "hglm"
+  return(hglm)
+}
