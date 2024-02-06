65c84e24887b5a666a09e7fce794023c8ee86066
Cindy Fang <cindyfang70@gmail.com>
2024-02-06 11:18:00 -0500

    add remaining output functions

--- R/model_outputs.R
+++ R/model_outputs.R
@@ -1,3 +1,15 @@
 predict.hglm <- function(hglm){
   warning("This is a placeholder until the function is implemented")
 }
+
+coef.hglm <- function(hglm){
+  warning("This is a placeholder until the function is implemented")
+}
+
+cov.hglm <- function(hglm){
+  warning("This is a placeholder until the function is implemented")
+}
+
+vcov.hglm <- function(hglm){
+  warning("This is a placeholder until the function is implemented")
+}
