bcf9cb75730951ec2481f1a158dac0b281779816
Cindy Fang <cindyfang70@gmail.com>
2024-02-06 11:23:54 -0500

    add roxygen2 documentation for the functions in model_outputs.R

--- R/model_outputs.R
+++ R/model_outputs.R
@@ -1,15 +1,39 @@
+#' predict.hglm
+#'
+#' @param hglm an S3 object of type hglm
+#'
+#' @return a warning
+#' @export
 predict.hglm <- function(hglm){
   warning("This is a placeholder until the function is implemented")
 }
 
+#' coef.hglm
+#'
+#' @param hglm an S3 object of type hglm
+#'
+#' @return a warning
+#' @export
 coef.hglm <- function(hglm){
   warning("This is a placeholder until the function is implemented")
 }
 
+#' cov.hglm
+#'
+#' @param hglm an S3 object of type hglm
+#'
+#' @return a warning
+#' @export
 cov.hglm <- function(hglm){
   warning("This is a placeholder until the function is implemented")
 }
 
+#' vcov.hglm
+#'
+#' @param hglm an S3 object of type hglm
+#'
+#' @return a warning
+#' @export
 vcov.hglm <- function(hglm){
   warning("This is a placeholder until the function is implemented")
 }
