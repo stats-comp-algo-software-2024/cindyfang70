822b7fb75e0ae17b0b480cf1acbea7426eab1b9d
Cindy Fang <cindyfang70@gmail.com>
2024-02-09 18:13:07 -0500

    implement bfgs and tests

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -5,9 +5,20 @@
 #'
 #' @return An object of class `hglm`
 #' @export
-hiper_glm <- function(design, outcome){
-  warning("This is a placeholder until the function is implemented")
+hiper_glm <- function(design, outcome, model, options){
   hglm <- list()
   class(hglm) <- "hglm"
+
+  if (model=="linear"){
+    if(missing(options)){
+      # assume pseudoinverse as default
+      betas_hat <- find.mle.pseudoinv(design, outcome)
+    }else if (options$mle_solver=="BFGS"){
+      # do bfgs
+      betas_hat <-find.mle.bfgs(design, outcome)
+    }
+
+  }
+  hglm$coefficients <- betas_hat
   return(hglm)
 }
