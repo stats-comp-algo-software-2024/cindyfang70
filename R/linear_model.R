38f22800bb7024aae54786bdc7a927613cd27097
Cindy Fang <cindyfang70@gmail.com>
2024-02-15 19:21:18 -0500

    remove print statement and unused computations

--- R/linear_model.R
+++ R/linear_model.R
@@ -1,5 +1,4 @@
 loglik.linear <- function(design, outcome, betas){
-  sigma_inv <- t(design) %*% design
   loglik <- 0.5*t(outcome - design %*% betas) %*% (outcome - design %*% betas)
   return(loglik)
 }
@@ -21,7 +20,6 @@ approxgrad.linear <- function(func, x, dx = .Machine$double.eps^(1/3)) {
     numerical_grad[[i]] <- (func(x_plus) - func(x_minus))/(2*dx)
   }
 
-  #numerical_grad <- (func(x + dx) - func(x - dx))/(2*dx)
   return(numerical_grad)
 }
 gaussian_logp <- function(X, x, betas) {
